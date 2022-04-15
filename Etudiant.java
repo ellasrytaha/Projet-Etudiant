a0bc1575de231dc60cb8d0926f2940cee7710d70
TAHA <ellasry0taha@gmail.com>
2022-04-15 11:22:47 +0000

    Redéfinir la méthode equals

--- Etudiant.java
+++ Etudiant.java
@@ -1,12 +1,18 @@
-public class Etudiant {
+public class Etudiant implements Comparable<Complexe> {
     private int id;
     private String name;
+    private double note;
 
-    private note;
+    public Etudiant(int id, String name, double note) {
+        note = 10;
+    }
 
-    public Etudiant(int id, String name, double note=10) {
-        this.id=id;
-        this.name=name;
-        this.note=note;
+    public String toString() {
+        return "Vnom:" + name + "Vnote:" + note;
+    }
 
+@Override
+	public boolean equals(Object obj) {
+		if( ! (obj instanceof Etudiant) )  return false;
+		return ((Etudiant)obj).id==this.id;
     }
