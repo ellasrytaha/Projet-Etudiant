b790540cb1c6ad9df65bc099e1521f4c70240792
TAHA <ellasry0taha@gmail.com>
2022-04-15 11:08:49 +0000

    creation des variables et du constructeur

--- Etudiant.java
+++ Etudiant.java
@@ -1,3 +1,12 @@
 public class Etudiant {
-    
-}
+    private int id;
+    private String name;
+
+    private note;
+
+    public Etudiant(int id, String name, double note=10) {
+        this.id=id;
+        this.name=name;
+        this.note=note;
+
+    }
